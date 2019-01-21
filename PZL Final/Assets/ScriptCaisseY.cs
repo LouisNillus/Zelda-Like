8d0f740a9c088f8a42518db54e9433c9e6dcd195
Louis Nillus <louis.nillus@gmail.com>
2019-01-21 23:52:13 +0100

    Box+Particles

---
+++ PZL Final/Assets/ScriptCaisseY.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCaisseY : MonoBehaviour
+{
+
+    public Transform positionJoueur;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            positionJoueur = collision.gameObject.GetComponent<Transform>();
+
+            if (positionJoueur.transform.position.y < this.transform.parent.transform.position.y)
+            {
+                this.transform.parent.transform.position = new Vector2(this.transform.parent.transform.position.x, this.transform.parent.transform.position.y + 0.05f);
+            }
+
+            if (positionJoueur.transform.position.y > this.transform.parent.transform.position.y)
+            {
+                this.transform.parent.transform.position = new Vector2(this.transform.parent.transform.position.x, this.transform.parent.transform.position.y - 0.05f);
+            }
+
+        }
+    }
+
+}
